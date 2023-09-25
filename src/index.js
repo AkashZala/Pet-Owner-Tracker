6c6dd425f4f3481bc8646603a6729be2889f0b6a
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-25 17:17:18 -0400

    display data on frontend, owner for each pet distinguished

--- src/index.js
+++ src/index.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
-const App = ()=> {
+const App = () => {
+  const [owners, setOwners] = useState([]);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get('/api/owners');
+      setOwners(response.data);
+    }
+    fetchUsers();
+  },[]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      const response = await axios.get('/api/pets');
+      setPets(response.data);
+    }
+    fetchPets();
+  }, []);
+  
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Owner-Pet Tracker</h1>
+      <main>
+        <div>
+          <h2>Owners ({ owners.length })</h2>
+          <ul>
+            {
+              owners.map(owner => {
+                const ownedPets = pets.filter(pet => owner.id === pet.owner_id);
+                return (
+                  <li key={owner.id}>
+                    {owner.name}
+                    ({ownedPets.length})
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+        <div>
+          <h2>Pets ({ pets.length })</h2>
+          <ul>
+            {
+              pets.map(pet => {
+                return (
+                  <li key={pet.id}>
+                    {pet.name}
+                    <ul>
+                      {
+                        owners.map(owner => {
+                          return (
+                            <li key={owner.id} className={ owner.id === pet.owner_id ? 'owner' : ''}>
+                              {owner.name}
+                            </li>
+                          );
+                        })
+                      }
+                    </ul>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+      </main>
+    </div>
   );
 };
 
